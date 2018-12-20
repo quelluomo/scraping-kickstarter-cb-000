cf4fa9f227d8b7d31f18995cd94b165f056333ef
Matteo Muscas <thatman.j@gmail.com>
2018-12-20 10:47:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,9 @@
 # require libraries/modules here
+require 'pry'
+
+html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
